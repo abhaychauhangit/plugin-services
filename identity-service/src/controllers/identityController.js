cba04c2f1b5b6774252020542b18104b6040fe96
Vaibhav Dixit <vaibhav84334@gmail.com>
2025-07-28 23:11:30 +0530

    Update identity-service/src/controllers/identityController.js

--- identity-service/src/controllers/identityController.js
+++ identity-service/src/controllers/identityController.js
@@ -5,22 +5,6 @@ import { validateRegistration, validatelogin } from "../utils/validation.js";
 
 
 
-// const setCookies = (accessToken, refreshToken, res) => {
-//   res.cookie("accessToken", accessToken, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       sameSite: "strict",
-//       maxAge: 15 * 60 * 1000,
-//   });
-
-//   res.cookie("refreshToken", refreshToken, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       sameSite: "strict",
-//       maxAge: 7 * 24 * 60 * 60 * 1000,
-//   });
-// }
-
 
 
 
